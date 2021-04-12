fd2481dfd7635d75f5eda7f0d1f70f47d76d00ac
Michael Arestad <marestad@gmail.com>
2021-04-12 17:42:12 -0600

    removed mini blog from home page

--- template-parts/footer/footer-content.php
+++ template-parts/footer/footer-content.php
@@ -13,26 +13,6 @@
 	<?php get_template_part( 'template-parts/footer/full-width-footer' ); ?>
 	<div class="footer-blog-and-widgets">
 		<div class="blog-and-search">
-			<?php if ( ! is_home() ) : ?>
-				<div class="tiny-mega-blog">
-					<h2><a href="<?php echo get_site_url() ?>/blog">Blog</a></h2>
-					<ul>
-						<?php
-						$recent_posts = wp_get_recent_posts(array(
-							'numberposts' => 10, // Number of recent posts thumbnails to display
-							'post_status' => 'publish' // Show only the published posts
-						));
-						foreach( $recent_posts as $post_item ) : ?>
-							<li>
-								<a href="<?php echo get_permalink($post_item['ID']) ?>">
-									<?php echo get_the_post_thumbnail($post_item['ID'], 'full'); ?>
-									<?php echo $post_item['post_title'] ?>
-								</a>
-							</li>
-						<?php endforeach; ?>
-					</ul>
-				</div>
-			<?php endif; ?>
 			<?php get_search_form(); ?>
 		</div>
 
