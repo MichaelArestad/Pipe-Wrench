7aa37c38fa2e215d829f7e0964ba23778c393cd9
Michael Arestad <marestad@gmail.com>
2021-04-13 08:00:44 -0600

    updated cover block

--- template-parts/footer/footer-content.php
+++ template-parts/footer/footer-content.php
@@ -11,6 +11,7 @@
 
 <?php if ( ! ( true === get_theme_mod( 'hide_site_footer', false ) && is_front_page() && is_page() ) ) : ?>
 	<?php get_template_part( 'template-parts/footer/full-width-footer' ); ?>
+
 	<div class="footer-blog-and-widgets">
 		<div class="blog-and-search">
 			<?php get_search_form(); ?>
